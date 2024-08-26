95f96e248ca86fc95ea442bcde3587a3a2ae8a5e
RwieRandom <hoff.mau.coding@gmail.com>
2024-08-26 21:12:10 +0200

    implemented widget.xml, WidgetProvider for a future widget

---
+++ app/src/main/java/de/your/yourshopdrop/WidgetProvider.kt
@@ -0,0 +1,30 @@
+package de.your.yourshopdrop
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class WidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+}
+
+fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+    val views = RemoteViews(context.packageName, R.layout.widget)
+
+    // Beispiel: Text im Widget ändern
+    views.setTextViewText(R.id.widget_text, "Widget aktualisiert!")
+
+    // Optional: Intent zum Öffnen der App bei Klick auf das Widget
+    val intent = Intent(context, MainActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE )
+    views.setOnClickPendingIntent(R.id.widget_text, pendingIntent)
+
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
